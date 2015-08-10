05e2ced12140266336240a42bcdf9e98b01f4094
Stefan Korner <stefan.korner@siemens.com>
2015-08-10 11:41:25 +0200

    setup initial package UTIL

---
+++ Makefile
@@ -0,0 +1,24 @@
+#******************************************************************************
+# (C) 2015, Stefan Korner, Austria                                            *
+#                                                                             *
+# The Space Java Library is free software; you can redistribute it and/or     *
+# modify it under the terms of the GNU Lesser General Public License as       *
+# published by the Free Software Foundation; either version 2.1 of the        *
+# License, or (at your option) any later version.                             *
+#                                                                             *
+# The Space Java Library is distributed in the hope that it will be useful,   *
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser     *
+# General Public License for more details.                                    *
+#******************************************************************************
+# Makefile                                                                    *
+#******************************************************************************
+
+all:
+	cd UTIL; make
+
+clean:
+	cd UTIL; make clean
+
+tst:
+	cd UTIL; make tst
---
+++ UTIL/DU.java
@@ -0,0 +1,24 @@
+//*****************************************************************************
+// (C) 2015, Stefan Korner, Austria                                           *
+//                                                                            *
+// The Space Java Library is free software; you can redistribute it and/or    *
+// modify it under the terms of the GNU Lesser General Public License as      *
+// published by the Free Software Foundation; either version 2.1 of the       *
+// License, or (at your option) any later version.                            *
+//                                                                            *
+// The Space Java Library is distributed in the hope that it will be useful,  *
+// but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser    *
+// General Public License for more details.                                   *
+//*****************************************************************************
+// Utilities - Data Unit                                                      *
+//*****************************************************************************
+package SpaceJavaLibrary.UTIL;
+
+public class DU
+{ 
+  public static void main(String[] args)
+  {
+    System.out.println("Hello , wordl!");
+  }
+}
